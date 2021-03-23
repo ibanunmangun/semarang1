be96a53e8eeb9caec9c6001dfd387b7e6a16d0e2
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-03-24 00:37:13 +0700

    Add files via upload

---
+++ data/Point_of_Interest_2.js
@@ -0,0 +1,26 @@
+var json_Point_of_Interest_2 = {
+"type": "FeatureCollection",
+"name": "Point_of_Interest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OID_": "1", "Name": "Masjid Menara \/ Layur", "Keterangan": "point of interest", "X": 110.4221597000001, "Y": -6.9663583 }, "geometry": { "type": "Point", "coordinates": [ 110.422159700000066, -6.966358299999968 ] } },
+{ "type": "Feature", "properties": { "OID_": "2", "Name": "Kota Lama Semarang", "Keterangan": "point of interest", "X": 110.4278611000001, "Y": -6.968139899999926 }, "geometry": { "type": "Point", "coordinates": [ 110.427861100000086, -6.968139899999926 ] } },
+{ "type": "Feature", "properties": { "OID_": "3", "Name": "Protestant Church in Western Indonesia Immanuel Semarang", "Keterangan": "point of interest", "X": 110.4274767000001, "Y": -6.9683487999999443 }, "geometry": { "type": "Point", "coordinates": [ 110.427476700000057, -6.968348799999944 ] } },
+{ "type": "Feature", "properties": { "OID_": "4", "Name": "Klenteng Besar TITD Tay Kak Sie", "Keterangan": "point of interest", "X": 110.4278718, "Y": -6.9740356 }, "geometry": { "type": "Point", "coordinates": [ 110.427871800000048, -6.974035599999979 ] } },
+{ "type": "Feature", "properties": { "OID_": "5", "Name": "Pasar Johar", "Keterangan": "point of interest", "X": 110.4247222000001, "Y": -6.9719443999999271 }, "geometry": { "type": "Point", "coordinates": [ 110.424722200000076, -6.971944399999927 ] } },
+{ "type": "Feature", "properties": { "OID_": "6", "Name": "Semarang Central Mosque", "Keterangan": "point of interest", "X": 110.422611, "Y": -6.9724755 }, "geometry": { "type": "Point", "coordinates": [ 110.422611, -6.972475499999973 ] } },
+{ "type": "Feature", "properties": { "OID_": "7", "Name": "Loenpia Mbak Lien", "Keterangan": "point of interest", "X": 110.4211819000001, "Y": -6.972929 }, "geometry": { "type": "Point", "coordinates": [ 110.421181900000079, -6.972929 ] } },
+{ "type": "Feature", "properties": { "OID_": "8", "Name": "Toko Oen", "Keterangan": "point of interest", "X": 110.4207626, "Y": -6.9734359999999356 }, "geometry": { "type": "Point", "coordinates": [ 110.420762600000046, -6.973436 ] } },
+{ "type": "Feature", "properties": { "OID_": "9", "Name": "Lawang Sewu", "Keterangan": "point of interest", "X": 110.410633, "Y": -6.9839249999999424 }, "geometry": { "type": "Point", "coordinates": [ 110.410633, -6.983925 ] } },
+{ "type": "Feature", "properties": { "OID_": "10", "Name": "Sam Poo Kong Temple", "Keterangan": "point of interest", "X": 110.3983688000001, "Y": -6.9959916999999336 }, "geometry": { "type": "Point", "coordinates": [ 110.398368800000071, -6.995991699999934 ] } },
+{ "type": "Feature", "properties": { "OID_": "11", "Name": "Rumah Sakit Umum Pusat Dr. Kariadi Semarang", "Keterangan": "fasilitas umum", "X": 110.407491, "Y": -6.994214 }, "geometry": { "type": "Point", "coordinates": [ 110.407491, -6.994214 ] } },
+{ "type": "Feature", "properties": { "OID_": "12", "Name": "Rumah Sakit Bhakti Wira Tamtama", "Keterangan": "fasilitas umum", "X": 110.407891, "Y": -6.986936 }, "geometry": { "type": "Point", "coordinates": [ 110.407891, -6.986936 ] } },
+{ "type": "Feature", "properties": { "OID_": "13", "Name": "Polrestabes Semarang", "Keterangan": "fasilitas umum", "X": 110.406162, "Y": -6.992381 }, "geometry": { "type": "Point", "coordinates": [ 110.406162, -6.992381 ] } },
+{ "type": "Feature", "properties": { "OID_": "14", "Name": "Pertamina COCO Gas Station Kaligarang Semarang", "Keterangan": "fasilitas umum", "X": 110.404925, "Y": -6.996245 }, "geometry": { "type": "Point", "coordinates": [ 110.404925, -6.996245 ] } },
+{ "type": "Feature", "properties": { "OID_": "15", "Name": "Masjid Jami Pekojan", "Keterangan": "fasilitas umum", "X": 110.428404, "Y": -6.972494 }, "geometry": { "type": "Point", "coordinates": [ 110.428404, -6.972494 ] } },
+{ "type": "Feature", "properties": { "OID_": "16", "Name": "Parkir Masjid Menara Layur", "Keterangan": "fasilitas umum", "X": 110.422075, "Y": -6.966937 }, "geometry": { "type": "Point", "coordinates": [ 110.422075, -6.966937 ] } },
+{ "type": "Feature", "properties": { "OID_": "17", "Name": "Parkir Kota Lama Semarang", "Keterangan": "fasilitas umum", "X": 110.426498, "Y": -6.96778 }, "geometry": { "type": "Point", "coordinates": [ 110.426498, -6.96778 ] } },
+{ "type": "Feature", "properties": { "OID_": "18", "Name": "Parkir Loenpia Mbak Lien dan Toko Oen", "Keterangan": "fasilitas umum", "X": 110.421124, "Y": -6.972762 }, "geometry": { "type": "Point", "coordinates": [ 110.421124, -6.972762 ] } },
+{ "type": "Feature", "properties": { "OID_": "19", "Name": "Parkir Lawang Sewu", "Keterangan": "fasilitas umum", "X": 110.410501, "Y": -6.984888 }, "geometry": { "type": "Point", "coordinates": [ 110.410501, -6.984888 ] } }
+]
+}
